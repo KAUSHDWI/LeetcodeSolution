bf78242818f97282c094d3a9ae31cc9edf21781c
Kaushlendra kumar Dwivedi <156951727+KAUSHDWI@users.noreply.github.com>
2026-05-04 23:39:03 +0530

    Time: 4 ms (92.31%), Space: 82.6 MB (76.79%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,15 +1,39 @@
-import java.util.Arrays;
+// import java.util.Arrays;
 
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         Arrays.sort(nums);  //  sort the array
+        
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] == nums[i - 1]) {  // check neighbors
+//                 return nums[i];
+//             }
+//         }
+        
+//         return -1; 
+//     }
+// }
 class Solution {
     public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);  //  sort the array
-        
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {  // check neighbors
-                return nums[i];
+        int slow = nums[0];
+        int fast = nums[0];
+
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (slow == fast) {
+                break;
             }
         }
-        
-        return -1; // duplicate always exists
+
+        int slow2 = nums[0];
+
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+
+        return slow;        
     }
 }
