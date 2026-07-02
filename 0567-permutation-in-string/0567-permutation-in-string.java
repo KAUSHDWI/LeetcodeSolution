36b17bdf8b94428e04ca4f53c35a99ec8d3cd013
Kaushlendra kumar Dwivedi <156951727+KAUSHDWI@users.noreply.github.com>
2026-07-02 22:33:50 +0530

    Time: 6 ms (87.47%), Space: 44.1 MB (49.42%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -6,23 +6,30 @@ class Solution {
         int n = s1.length();
         int m = s2.length();
 
-        if (n > m) return false;
-
-        int[] s1Freq = new int[26];
-
-        for (int i = 0; i < n; i++) {
-            s1Freq[s1.charAt(i) - 'a']++;
+        if (n > m) {
+            return false;
         }
 
-        for (int i = 0; i <= m - n; i++) {
+        int[] s1Freq = new int[26];
+        int[] wFreq = new int[26];
 
-            int[] windowFreq = new int[26];
+        // Frequency of s1
+        for (int i = 0; i < n; i++) {
+            s1Freq[s1.charAt(i) - 'a']++;
+            wFreq[s2.charAt(i) - 'a']++;
+        }
 
-            for (int j = i; j < i + n; j++) {
-                windowFreq[s2.charAt(j) - 'a']++;
-            }
+        if (Arrays.equals(s1Freq, wFreq)) {
+            return true;
+        }
 
-            if (Arrays.equals(s1Freq, windowFreq)) {
+        for (int i = n; i < m; i++) {
+
+            wFreq[s2.charAt(i) - 'a']++;
+
+            wFreq[s2.charAt(i - n) - 'a']--;
+
+            if (Arrays.equals(s1Freq, wFreq)) {
                 return true;
             }
         }
