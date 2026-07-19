3b07ad59877980a8f208aaf87da1c96a6b9b6b39
Kaushlendra kumar Dwivedi <156951727+KAUSHDWI@users.noreply.github.com>
2026-07-19 22:50:36 +0530

    Time: 9 ms (61.35%), Space: 48.6 MB (92.15%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int [n];
+        Arrays.sort(nums);
+        int l =(n-1)/2;
+        int r = n-1;
+        for(int i =0;i<n;i++){
+            if(i%2==0){
+                ans[i] = nums[l];
+                l--;
+            }else{
+                ans[i] = nums[r];
+                r--;
+            }
+        }
+        for(int i =0;i<n;i++){
+            nums[i] = ans[i];
+        }
+
+    }
+}
