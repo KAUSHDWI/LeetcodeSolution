5cea18fee05e43b22f264c29d5a591432bcd3264
Kaushlendra kumar Dwivedi <156951727+KAUSHDWI@users.noreply.github.com>
2026-07-20 22:22:34 +0530

    Time: 62 ms (5.09%), Space: 49.2 MB (14.48%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -1,22 +1,58 @@
 class Solution {
     public void wiggleSort(int[] nums) {
         int n = nums.length;
-        int[] ans = new int [n];
-        Arrays.sort(nums);
-        int l =(n-1)/2;
-        int r = n-1;
-        for(int i =0;i<n;i++){
-            if(i%2==0){
-                ans[i] = nums[l];
-                l--;
-            }else{
-                ans[i] = nums[r];
-                r--;
+        int median = findKth(nums, (n + 1) / 2);
+
+        int left = 0, i = 0, right = n - 1;
+
+        while (i <= right) {
+            if (nums[index(i, n)] > median) {
+                swap(nums, index(left++, n), index(i++, n));
+            } else if (nums[index(i, n)] < median) {
+                swap(nums, index(i, n), index(right--, n));
+            } else {
+                i++;
             }
         }
-        for(int i =0;i<n;i++){
-            nums[i] = ans[i];
+    }
+
+    private int index(int i, int n) {
+        return (1 + 2 * i) % (n | 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private int findKth(int[] nums, int k) {
+        int left = 0, right = nums.length - 1;
+        k = nums.length - k;
+
+        while (left <= right) {
+            int pivot = partition(nums, left, right);
+            if (pivot == k)
+                return nums[pivot];
+            else if (pivot < k)
+                left = pivot + 1;
+            else
+                right = pivot - 1;
+        }
+        return -1;
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int p = left;
+
+        for (int i = left; i < right; i++) {
+            if (nums[i] < pivot) {
+                swap(nums, i, p++);
+            }
         }
 
+        swap(nums, p, right);
+        return p;
     }
 }
