f781a8310ba22a1f60bddcd9827cc94256aa7a5f
Kaushlendra kumar Dwivedi <156951727+KAUSHDWI@users.noreply.github.com>
2026-08-20 10:42:05 +0530

    Time: 8 ms (49.76%), Space: 46.4 MB (54.4%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -1,11 +1,14 @@
 
 class Solution {
     public boolean divideArray(int[] nums) {
+        // step 1 = Make an Hashmap and count the feq of elemnt 
+
         HashMap<Integer, Integer> feq = new HashMap<>();
         for (int num : nums) {
             feq.put(num, feq.getOrDefault(num, 0) + 1);
         }
-        
+
+       // step 2 = if frq of elemnt is not divided by 2 so it will return false
         for (int a : feq.values()) {
             if (a % 2 != 0) {
                 return false;
