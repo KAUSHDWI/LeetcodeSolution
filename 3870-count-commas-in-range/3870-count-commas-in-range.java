76a6bee0095caa81475e437853b7ed1371bc1bb8
Kaushlendra kumar Dwivedi <156951727+KAUSHDWI@users.noreply.github.com>
2026-09-08 23:14:07 +0530

    Time: 1 ms (83.65%), Space: 42.2 MB (93.72%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,11 +1,15 @@
 
 class Solution {
     public int countCommas(int n) {
-        if (n < 1000) {
-            return 0;
-        }
+        // if (n < 1000) {
+        //     return 0;
+        // }
 
-        return n - 999;
+        // return n - 999;
+
+
+
+          return Math.max(0, n - 999);
     }
 }
 
