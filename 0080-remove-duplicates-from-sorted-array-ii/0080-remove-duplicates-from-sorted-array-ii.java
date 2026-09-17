83d828ad27e003134f0037f0278e581c8136b326
Kaushlendra kumar Dwivedi <156951727+KAUSHDWI@users.noreply.github.com>
2026-09-17 21:17:09 +0530

    Time: 0 ms (100%), Space: 48.9 MB (15.23%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,19 +1,28 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k =0;
-        int count =0;
-        for(int i =0;i<nums.length;i++){
-            if(i==0 || nums[i] != nums[i-1]){
-                count = 1;
-            }else{
-                count++;
-            }
-            if(count <= 2){
+        // int k =0;
+        // int count =0;
+        // for(int i =0;i<nums.length;i++){
+        //     if(i==0 || nums[i] != nums[i-1]){
+        //         count = 1;
+        //     }else{
+        //         count++;
+        //     }
+        //     if(count <= 2){
+        //         nums[k] = nums[i];
+        //         k++;
+        //     }
+        // }
+        // return k;
+        
+        // two pinter 
+        int k =2;
+        for(int i =2;i<nums.length;i++){
+            if(nums[i] != nums[k - 2]){
                 nums[k] = nums[i];
                 k++;
             }
         }
         return k;
-        
     }
 }
