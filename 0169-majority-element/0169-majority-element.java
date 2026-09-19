45de56bec56ee312515ae48b142a7545cb2f14a8
Kaushlendra kumar Dwivedi <156951727+KAUSHDWI@users.noreply.github.com>
2026-09-19 18:25:11 +0530

    Time: 21 ms (10.43%), Space: 55.8 MB (21.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,8 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
         //BF
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
+
+
+
+// Hashmap
+HashMap<Integer,Integer>map = new HashMap<>();
+for(int num : nums){
+    map.put(num,map.getOrDefault(num,0) +1);
+    if(map.get(num)> nums.length/2){
+        return num;
+    }
+}
+return -1;
+        //Boyer- Moore Voting Algorithm 
+
 
     }
 }
