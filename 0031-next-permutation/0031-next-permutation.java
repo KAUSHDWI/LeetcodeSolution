9536b39e4005aaf90b7ba4fe6a508af970d5ee55
Kaushlendra kumar Dwivedi <156951727+KAUSHDWI@users.noreply.github.com>
2026-09-23 21:09:27 +0530

    Time: 0 ms (100%), Space: 44.5 MB (84.13%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,45 +1,42 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int ind1=-1;
-        int ind2=-1;
-        // step 1 find breaking point 
-        for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind1=i;
+
+        int n = nums.length;
+        int pivot = -1;
+
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] < nums[i + 1]){
+                pivot = i;
                 break;
             }
         }
-        // if there is no breaking  point 
-        if(ind1==-1){
-            reverse(nums,0);
-        }
-        
-        else{
-            // step 2 find next greater element and swap with ind2
-            for(int i=nums.length-1;i>=0;i--){
-                if(nums[i]>nums[ind1]){
-                    ind2=i;
-                    break;
-                }
-            }
 
-            swap(nums,ind1,ind2);
-            // step 3 reverse the rest right half
-            reverse(nums,ind1+1);
+        if(pivot == -1){
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        for(int i = n - 1; i > pivot; i--){
+            if(nums[i] > nums[pivot]){
+                swap(nums, i, pivot);
+                break;
+            }
+        }
+
+        reverse(nums, pivot + 1, n - 1);
+    }
+
+    void reverse(int[] nums, int l, int r){
+        while(l < r){
+            swap(nums, l, r);
+            l++;
+            r--;
         }
     }
-    void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-    }
-    void reverse(int[] nums,int start){
-        int i=start;
-        int j=nums.length-1;
-        while(i<j){
-            swap(nums,i,j);
-            i++;
-            j--;
-        }
+
+    void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
