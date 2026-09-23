3ddf90c663feddb86a9439f0927690b77bd06757
Kaushlendra kumar Dwivedi <156951727+KAUSHDWI@users.noreply.github.com>
2026-09-24 00:47:50 +0530

    Time: 5 ms (72.05%), Space: 43.4 MB (57.99%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,19 +1,42 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int ans =0;
+        // Arrays.sort(citations);
+    //     int ans =0;
         
-        for(int h =0;h <= citations.length;h++){
-            int count = 0;
-            for(int i =0;i<citations.length;i++){
-                if(citations[i] >= h){
-                    count++;
-                }
-            }
-            if(count >= h){
-                ans = h;
+    //     for(int h =0;h <= citations.length;h++){
+    //         int count = 0;
+    //         for(int i =0;i<citations.length;i++){
+    //             if(citations[i] >= h){
+    //                 count++;
+    //             }
+    //         }
+    //         if(count >= h){
+    //             ans = h;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
+    // sorting 
+    
+        
+        Arrays.sort(citations);
+
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            
+            int h = n - i;
+
+            if (citations[i] >= h) {
+                return h;
             }
         }
-        return ans;
+
+        return 0;
     }
-}
+
+    }
+    
+
